fc45d7ba074e4d78aba7d4c842750df6a4f8d2fe
mskian <10300271+mskian@users.noreply.github.com>
2023-12-12 00:09:14 +0530

    update site url

--- _config.js
+++ _config.js
@@ -3,6 +3,7 @@ import blog from "https://raw.githubusercontent.com/lumeland/theme-simple-blog/m
 
 const port = Deno.env.get("PORT") ? Number(Deno.env.get("PORT")) : 8000;
 const site = lume({
+    location: new URL("https://foss.sanweb.info/"),
     server: {
         port: port
     }
